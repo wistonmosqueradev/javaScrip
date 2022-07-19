a654e0c636716f82ee798381f6527bea66a0c4b6
wistonmosqueradev <wistonmosqueradev@gmail.com>
2022-07-19 06:55:19 -0500

    ejercicio de clase 2 de js

---
+++ clase1/scripts/app.js
@@ -0,0 +1,34 @@
+
+
+// let nombre=prompt("ingrese su nombre") ;
+// let apellido=prompt("ingrese su apellido");
+// console.log(nombre+apellido);
+
+// esta es una constante 
+const email="wistonmosqueradev@gmail.com"
+console.log(`el correo es:${email}`);
+
+// este es un ejemplo de undefined
+let correo;
+console.log(`el correo es:${correo}`);
+
+
+
+let nombre1=prompt("cual es tu nombre");
+let apellido1=prompt("cual es tu apellido");
+
+alert(`hola ${nombre1} ${apellido1}, le damos la bienvenida`);
+
+let gusto1=prompt("cual es tu primer gusto");
+let gusto2=prompt("cual es tu primer gusto");
+let gusto3=prompt("cual es tu primer gusto");
+alert(`${gusto1} ${gusto2} ${gusto3}`);
+
+
+let nombre1=prompt("cual es tu nombre?");
+let apellido1=prompt("cual es tu apellido?");
+let edad=prompt=("cual es tu edad?");
+let nacinalidad=prompt("cual es tu nacionalidad?");
+let numeroIdentificacion=prompt("¿Cuál es tu número de identificación personal?")
+
+alert (`${nombre1} ${apellido1} ${edad} ${nacinalidad} ${numeroIdentificacion}`)
