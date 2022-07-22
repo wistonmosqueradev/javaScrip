f77c4ae6f9a0a8ac72900370de5afbfb538e0f5d
wistonmosqueradev <wistonmosqueradev@gmail.com>
2022-07-22 09:56:19 -0500

    funciones de numeros

---
+++ clase4/assets/funcionesNumeros.js
@@ -0,0 +1,17 @@
+console.log(Math.E)//
+console.log(Math.PI)
+console.log(Math.pow(3,2))
+
+let numero1=30;
+let numero2=82;
+console.log(Math.min(numero1,numero2))
+console.log(Math.max(numero1,numero2))
+
+const numeroPi=4.14567
+
+console.log(Math.round(numeroPi))//devuelve el numero mas ercano 
+
+const numeros=[0,20,10,5,30]
+console.log(typeof numeros)
+console.log(`el numero menor es: ${Math.min(...numeros)}`)
+console.log(`el numero mayor es: ${Math.max(...numeros)}`)
